caaec168400f8a94ec6992248d96c5a923d44c77
Иван Шиповский <ivanscemt@yandex.ru>
2023-08-13 07:57:34 +0300

    DetailView

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,36 +2,53 @@ from .forms import BirthdayForm
 
 from .models import Birthday
 
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView, ListView, UpdateView, DeleteView, DetailView)
 
 from django.urls import reverse_lazy
+
+from .utils import calculate_birthday_countdown
 
 
 class BirthdayMixin:
     model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayFormMixin:
     form_class = BirthdayForm
     template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list')
 
 
 class BirthdayListView(ListView):
-    # Указываем модель, с которой работает CBV...
     model = Birthday
-    # ...сортировку, которая будет применена при выводе списка объектов:
     ordering = 'id'
-    # ...и даже настройки пагинации:
     paginate_by = 5
 
 
-class BirthdayCreateView(BirthdayMixin, CreateView):
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
     pass
 
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
     pass
 
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
     model = Birthday
-    template_name = 'birthday/birthday.html'
     success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
